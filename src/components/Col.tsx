07270d0c1c18bac517875ee6237a112764a0aad4
Kanchan <bn.kanchan@gmail.com>
2020-07-27 14:37:47 +1000

    small files combined into one

--- src/components/Col.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FC } from "react";
-
-export const Col: FC<{}> = () => {
-    return (
-    <>
-        <div style={{height:"7em", width:"7em", margin: "0.1em", backgroundColor:"lightGrey", borderRadius:"0.3em"}}></div>
-    </>);
-}
